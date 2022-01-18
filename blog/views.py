8fb1ca1096a026e1d9a8d1f0c8665c65205bc725
kyk <kykyung810@gmail.com>
2022-01-18 16:59:39 +0900

    글 삭제기능

--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 from .forms import PostForm
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request,'blog/post_list.html',{'posts':posts})
 
 def post_detail(request, pk):
@@ -37,3 +37,8 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('post_list')
